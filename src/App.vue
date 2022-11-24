c273c9f54834c49e51da11a2a63eb2440cbb2e73
viccam <viccam@student.chalmers.se>
2022-11-24 11:01:42 +0100

    Implements basic unmarked google map

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Welcome to Dentistimo Frontend</h1>
+  <div ref="mapDiv" style="width: 100%; height: 80vh" />
 </template>
+
+<script>
+/* eslint-disable no-undef */
+import { ref, onMounted } from 'vue'
+import { Loader } from '@googlemaps/js-api-loader'
+const MAP_API_KEY = 'AIzaSyDrWi2jVS_VxFVJ2P5cOfwZUPpeykLLOBg'
+export default {
+  name: 'App',
+  setup() {
+    const loader = new Loader({ apiKey: MAP_API_KEY })
+    const mapDiv = ref(null)
+    let map = ref(null)
+    onMounted(async () => {
+      await loader.load()
+      map.value = new google.maps.Map(mapDiv.value, {
+        center: { lat: 57.7098281, lng: 11.9776431 },
+        zoom: 13,
+      })
+    })
+    return { mapDiv }
+  },
+}
+</script>
