d0e1cbb44059bcc1fc845a2e094b6956a6c8fe2a
viccam <viccam@student.chalmers.se>
2022-11-25 11:25:38 +0100

    add for-loop that creates markers

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
 </template>
 
 <script>
-/* eslint-disable no-undef */
-
 import { ref, onMounted } from 'vue'
 import { Loader } from '@googlemaps/js-api-loader'
 const MAP_API_KEY = 'AIzaSyDrWi2jVS_VxFVJ2P5cOfwZUPpeykLLOBg'
@@ -13,7 +11,39 @@ export default {
   setup() {
     const loader = new Loader({ apiKey: MAP_API_KEY })
     const mapDiv = ref(null)
+    // HTML code example that should include data of clincs from database that
+    // should be loaded onMounted
+    const clinics = [
+      '<h3> name of clinic </h3>' +
+        '<div>maybe some description</div>' +
+        '<button onclick="AFunction()">Button that takes you to the booking page of the specific clinic</button>',
+
+      '<h3> name of another clinic </h3>' +
+        '<div>another description</div>' +
+        '<button onclick="AnotherFunction()">Button that takes you to the booking page of the specific clinic</button>',
+    ]
+
+    // hardcoded cordinates for clinics, should request data from database and put here
+    var cords = [
+      {
+        LatLng: [
+          {
+            lat: 57.7198281,
+            lng: 11.9876431,
+          },
+        ],
+      },
+      {
+        LatLng: [
+          {
+            lat: 57.7098281,
+            lng: 11.9876431,
+          },
+        ],
+      },
+    ]
     let map = ref(null)
+
     // loads the map and puts it in the div element with ref="mapDiv"
     onMounted(async () => {
       await loader.load()
@@ -21,11 +51,21 @@ export default {
         center: { lat: 57.7098281, lng: 11.9776431 },
         zoom: 13,
       })
-      // creates a hardcoded custom marker, will be used later for showing all the different clinics
-      const marker = new google.maps.Marker({
-        position: { lat: 57.7098281, lng: 11.9776431 },
-        map: map.value,
-      })
+      const infoWindow = new google.maps.InfoWindow()
+      // creates custom markers, will be used later for showing all the different clinics
+      for (var i = 0; i < cords.length; i++) {
+        const marker = new google.maps.Marker({
+          position: cords[i].LatLng[0],
+          map: map.value,
+          title: clinics[i],
+        })
+        // Shows the title of the marker via a infoWindow when you click on the marker
+        marker.addListener('click', () => {
+          infoWindow.close()
+          infoWindow.setContent(marker.getTitle())
+          infoWindow.open(marker.getMap(), marker)
+        })
+      }
     })
     return { mapDiv }
   },
