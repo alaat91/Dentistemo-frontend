0a26bd5785bde2e34e5a05ff73e38955f8b97878
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-12-06 15:16:32 +0100

    Create basic appearence for the profile and timeslot page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,17 @@
 <template>
 <div>
-    <button> B </button>
+    <h1> Profile </h1>
+     <!-- A simple button -->
+    <button> Back </button> 
+    <!-- User information inputs-->
+    <!--TODO: create input component for reuse + Implement Grid -->
+    <label for="name">Enter First Name:</label>
+<input type="text" id="name" name="name" required
+       minlength="4" maxlength="8" size="10">
+
 </div>
 </template>
+
 
 <script>
 export default {
@@ -10,6 +19,16 @@ export default {
 }
 </script>
 
+
 <style scoped>
+label {
+    display: block;
+    font: 1rem 'Fira Sans', sans-serif;
+}
+
+input,
+label {
+    margin: .4rem 0;
+}
 
 </style>
