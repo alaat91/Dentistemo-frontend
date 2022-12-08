deb270dee3eb0d634573f689407ee7b26f3b7184
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-12-08 11:35:22 +0100

    create timeslot component, UI for the weekdays, fix routing Co-authored-by: viccam viccam@student.chalmers.se

---
+++ src/components/TimeslotItem.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+  <b-dropdown id="dropdown-dropright" dropright text="Drop-Right" variant="primary" class="m-2">
+    <b-dropdown-item href="#">Book this timeslot</b-dropdown-item>
+  </b-dropdown>
+</div>
+</template>
