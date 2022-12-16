effb7a901fd8bc3880a62998051dd1b1bab797a9
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-12-16 10:41:18 +0100

    Add sub-header to landing page component

--- src/components/LandingItem.vue
+++ src/components/LandingItem.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="topnav" id="myTopnav">
       <h1 id="header">Welcome to Dentistemo</h1>
+      <h6 id="sub-header">"A good life starts with good teeth"-Dr.Shariq</h6>
     </div>
 </template>
 
