4d450b2d82d28cff75d4fe4616f7785e3626526a
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-12-16 10:50:58 +0100

    Add CSS styling to header in landing page component

--- src/components/LandingItem.vue
+++ src/components/LandingItem.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <div class="topnav" id="myTopnav">
       <h1 id="header">Welcome to Dentistemo</h1>
       <h6 id="sub-header">"A good life starts with good teeth"-Dr.Shariq</h6>
@@ -9,9 +10,9 @@
              <router-link to="/signup" class="signup">Signup</router-link>
      </b-row>
     </div>
+  <div style="padding-left: 16px"></div>
+</div>
 </template>
-
-
 
 
 <script>
@@ -19,7 +20,11 @@ export default {}
 </script>
 
 
-
-
 <style scoped>
+#header{
+    text-shadow: 4px 4px 12px white;
+    font-size: 60px;
+     text-align: center;
+    
+}
 </style>
