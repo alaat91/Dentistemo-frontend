6931b05694821f696b7547599a4862df558bd739
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-12-16 10:45:17 +0100

    Connect login page to landing page through routerlink

--- src/components/LandingItem.vue
+++ src/components/LandingItem.vue
@@ -2,6 +2,9 @@
     <div class="topnav" id="myTopnav">
       <h1 id="header">Welcome to Dentistemo</h1>
       <h6 id="sub-header">"A good life starts with good teeth"-Dr.Shariq</h6>
+      <b-row>
+             <router-link to="/login" class="login">Login</router-link>
+           </b-row>
     </div>
 </template>
 
