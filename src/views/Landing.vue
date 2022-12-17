0e7d0735b3f27714dfabf1386977357f796e2493
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-12-17 12:40:10 +0100

    Add CSS styling for the background image in landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,10 @@
 <template>
 <body>
+   <b-container fluid class="container1">
 <div>
   <b-img src="https://static.showit.co/1200/NVyu1HUlT1i7YcsOdIg2Sg/84619/fmc-dental-clinic-background.png" class="min-vh-100"  fluid-grow alt="Responsive image"></b-img>
 </div>
+   </b-container>
 </body>
 </template>
 
@@ -13,6 +15,10 @@
    text-shadow: 4px 4px 8px white;
    width: 100%;
    height: 100%;
+  
+}
+.min-vh-100{
+  opacity: 0.9;
   
 }
 </style>
