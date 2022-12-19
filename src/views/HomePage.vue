e35b971bc6b9d5809df891fd71fd5ecb73d049cf
Alaa_Taleb <alaat@student.chalmers.se>
2022-12-19 16:34:36 +0100

    retriving all clinics from the back-end instead of having them as dummy data

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,7 @@
         <ul>
           <h4>Available Clinics</h4>
           <clinics-list
-            v-for="dentist in dentists"
+            v-for="dentist in clinics"
             :key="dentist.id"
             :clinicId="dentist.id"
             :clinicName="dentist.name"
@@ -24,92 +24,15 @@
 </template>
 
 <script>
+import { API } from '../config/api'
 import ClinicsList from '../components/ClinicsList.vue'
-//import ClinicsList from '../components/ClinicsList.vue'
 import Map from '../components/Map.vue'
 import TheNavigation from '../components/TheNavigation.vue'
 export default {
   components: { Map, TheNavigation, ClinicsList },
   data() {
     return {
-      dentists: [
-        {
-          id: 1,
-          name: 'Your Dentist',
-          owner: 'Dan Tist',
-          dentists: 3,
-          address: 'Spannmålsgatan 20',
-          city: 'Gothenburg',
-          coordinate: {
-            longitude: 11.969388,
-            latitude: 57.707619,
-          },
-          openinghours: {
-            monday: '9:00-17:00',
-            tuesday: '8:00-17:00',
-            wednesday: '7:00-16:00',
-            thursday: '9:00-17:00',
-            friday: '9:00-15:00',
-          },
-        },
-        {
-          id: 2,
-          name: 'Tooth Fairy Dentist',
-          owner: 'Tooth Fairy',
-          dentists: 1,
-          address: 'Slottskogen',
-          city: 'Gothenburg',
-          coordinate: {
-            longitude: 11.942625,
-            latitude: 57.685255,
-          },
-          openinghours: {
-            monday: '7:00-19:00',
-            tuesday: '7:00-19:00',
-            wednesday: '7:00-19:00',
-            thursday: '7:00-19:00',
-            friday: '7:00-19:00',
-          },
-        },
-        {
-          id: 3,
-          name: 'The Crown',
-          owner: 'Carmen Corona',
-          dentists: 2,
-          address: 'Lindholmsallén 19',
-          city: 'Gothenburg',
-          coordinate: {
-            longitude: 11.940386,
-            latitude: 57.709872,
-          },
-          openinghours: {
-            monday: '6:00-15:00',
-            tuesday: '8:00-17:00',
-            wednesday: '7:00-12:00',
-            thursday: '7:00-17:00',
-            friday: '8:00-16:00',
-          },
-        },
-        {
-          id: 4,
-          name: 'Lisebergs Dentists',
-          owner: 'Glen Hysén',
-          dentists: 3,
-          address: 'Liseberg',
-          city: 'Gothenburg',
-          coordinate: {
-            longitude: 11.991153,
-            latitude: 57.694723,
-          },
-          openinghours: {
-            monday: '10:00-18:00',
-            tuesday: '10:00-18:00',
-            wednesday: '10:00-18:00',
-            thursday: '10:00-18:00',
-            friday: '10:00-18:00',
-          },
-        },
-      ],
+      clinics: [],
       items: [
         {
           text: 'Home',
@@ -129,6 +52,14 @@ export default {
   provide() {
     return {
       dentists: this.dentists,
+    }
+  },
+  mounted: async function () {
+    try {
+      const res = await API.get('/clinics/')
+      this.clinics = res.data
+    } catch (err) {
+      console.error(err)
     }
   },
 }
