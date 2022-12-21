4818d0ac01bf6a74afa95957808a068ec685096c
balesic <balesic@student.chalmers.se>
2022-12-21 14:12:35 +0100

    Correct typo in Dentistimo

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,33 +1,37 @@
 <template>
-<body>
-   <b-container fluid class="container1">
-       <landing-item></landing-item>
-<div>
-  <b-img src="https://static.showit.co/1200/NVyu1HUlT1i7YcsOdIg2Sg/84619/fmc-dental-clinic-background.png" class="min-vh-100"  fluid-grow alt="Responsive image"></b-img>
-</div>
-   </b-container>
-</body>
+  <body>
+    <b-container fluid class="container1">
+      <landing-item></landing-item>
+      <div>
+        <b-img
+          src="https://static.showit.co/1200/NVyu1HUlT1i7YcsOdIg2Sg/84619/fmc-dental-clinic-background.png"
+          class="min-vh-100"
+          fluid-grow
+          alt="Responsive image"
+        ></b-img>
+      </div>
+    </b-container>
+  </body>
 </template>
 
 <script>
-  import LandingItem from '../components/LandingItem.vue'
-  export default {
-    components: {
-      LandingItem,
-    },
-  }
-  </script>
+import LandingItem from '../components/LandingItem.vue'
+export default {
+  components: {
+    LandingItem,
+  },
+}
+</script>
 
 <style scoped>
-.container{
-   background-color: lightskyblue;
-   text-align: center;
-   text-shadow: 4px 4px 8px white;
-   width: 100%;
-   height: 100%;
-  
+.container {
+  background-color: lightskyblue;
+  text-align: center;
+  text-shadow: 4px 4px 8px white;
+  width: 100%;
+  height: 100%;
 }
-.min-vh-100{
+.min-vh-100 {
   opacity: 0.9;
   object-fit: cover;
 }
