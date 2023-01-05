63680363a7c9632b8fb30911687ae1383a9631f9
Labiba Karar Eshaba <guseshka@student.gu.se>
2023-01-05 15:12:55 +0100

    Implement footer component for the landing page in frontend

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,24 @@
 <template>
-    
+    <div class="footer">
+            <div class="container">     
+                <div class="row">                       
+                    <div class="col-lg-4 col-sm-4 col-xs-12">
+                        <div class="single_footer">
+                            <h4>Contact Us</h4>
+                            <ul> 
+                                <li><a href="#">Email: dentistimo@hotmail.com</a></li>
+                                <li><a href="#">Phone: +467778888</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                         
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                        <p class="copyright">Copyright © 2023 Dentistimo.</p>
+                    </div>                
+                </div>               
+            </div>
+        </div>
 </template>
