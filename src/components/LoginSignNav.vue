a044c5f52c6d9589449b40ad6aefa45a7ba68bd5
Labiba Karar Eshaba <guseshka@student.gu.se>
2023-01-07 11:32:54 +0100

    Implement landing button in the login-signup navbar

--- src/components/LoginSignNav.vue
+++ src/components/LoginSignNav.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <nav>
+    <div class="topnav" id="myTopnav">
+      <h1>Dentistimo</h1>
+     <b-row class="row" cols="1" cols-sm="2" cols-md="3" cols-lg="4">
+       <b-col class="navbtn"><router-link :to="{name:'landing'}">Landing</router-link></b-col>
+    </b-row>
+    </div>
+  </nav>
 </template>
 
 
