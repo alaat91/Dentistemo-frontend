ae4cb48997e8443098dbef18a1b5b930b494781c
Labiba Karar Eshaba <guseshka@student.gu.se>
2023-01-07 10:45:20 +0100

    Remove existing navbar from the profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,32 +1,7 @@
 <template>
   <div>
     <!-- Header -->
-    <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
-      <b-container class="container">
-        <div class="header-body text-center mb-7">
-          <b-row class="justify-content-center">
-            <b-col xl="5" lg="6" md="8" class="px-5">
-              <h1 class="text-black">Your Profile.</h1>
-            </b-col>
-          </b-row>
-        </div>
-      </b-container>
-      <div class="separator separator-bottom separator-skew zindex-100">
-        <svg
-          x="0"
-          y="0"
-          viewBox="0 0 2560 100"
-          preserveAspectRatio="none"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <polygon
-            class="fill-default"
-            points="2560 0 2560 100 0 100"
-          ></polygon>
-        </svg>
-      </div>
-    </div>
+    
     <!-- Page content -->
     <b-overlay class="mt--8">
       <b-row class="justify-content-center">
