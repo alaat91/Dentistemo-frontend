695dcb95236f0d6ded4fe55833d2cfc67b687267
Labiba Karar Eshaba <guseshka@student.gu.se>
2023-01-07 15:19:09 +0100

    Implement logout button in the navbar for authorized pages

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -2,11 +2,12 @@
   <nav>
     <div class="topnav" id="myTopnav">
       <h1>Dentistimo</h1>
-     <b-row class="row" cols="1" cols-sm="2" cols-md="3" cols-lg="4">
+     <b-row class="row" cols="1" cols-sm="2" cols-md="3" cols-lg="5">
        <b-col class="navbtn"><router-link class="navbtn" :to="{name:'landing'}"><h5>Landing</h5></router-link></b-col>
        <b-col><router-link :to="{name:'home'}"><h5>Home</h5></router-link></b-col>
        <b-col><router-link :to="{name:'profile'}"><h5>Profile</h5></router-link></b-col>
        <b-col><router-link :to="{name:'myBooking'}"><h5>My Booking</h5></router-link></b-col>
+       <b-col><router-link :to="{name:''}"><h5>Logout</h5></router-link></b-col>
     </b-row>
     </div>
   </nav>
